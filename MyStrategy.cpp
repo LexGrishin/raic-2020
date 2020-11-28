878546ddfc37b7c137e9416856196127b7db57af
Alexey Grishin <a.y.grishin@yandex.ru>
2020-11-28 14:30:12 +0300

    try to draw some text

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -5,6 +5,13 @@ MyStrategy::MyStrategy() {}
 
 Action MyStrategy::getAction(const PlayerView& playerView, DebugInterface* debugInterface)
 {
+    Color red{255, 0, 0, 255};
+    auto x = playerView.mapSize / 10;
+    auto point = std::make_shared<Vec2Float>(x, x);
+    auto debugText = std::make_shared<DebugData::PlacedText>(ColoredVertex(point, Vec2Float(0, 0), red), "hellow world!", 0, 200);
+    debugInterface->send(DebugCommand::Clear());
+    debugInterface->send(DebugCommand::Add(debugText));
+    debugInterface->getState();
     return Action(std::unordered_map<int, EntityAction>());
 }
 
